a823555f22b0e028145f61fc258bf12bdb957003
Roxanica123 <irimia.andreea.roxana@gmail.com>
2020-12-10 17:37:57 +0200

    Added TwitterRequest class

---
+++ twitter_request.py
@@ -0,0 +1,36 @@
+import requests
+
+from tweeter_credentials import bearer_token
+
+QUERY = "query="
+TWEET_FIELDS = "tweet.fields="
+NEXT_TOKEN = "next_token="
+
+
+class TwitterRequest:
+    def __init__(self, hashtag, max_results=10):
+        self.hashtag = hashtag
+        self.max_results = max_results
+        self.url = self.build_url()
+        self.headers = self.build_headers()
+
+    def build_url(self, next_token=None):
+        query = QUERY + self.hashtag
+        tweet_fields = TWEET_FIELDS + "geo"
+        url = "https://api.twitter.com/2/tweets/search/recent?{}&{}".format(
+            query, tweet_fields
+        )
+        if next_token is not None:
+            url = url + "&{}".format(NEXT_TOKEN + next_token)
+        return url
+
+    def send_request(self):
+        response = requests.request("GET", self.url, headers=self.headers)
+        if response.status_code != 200:
+            raise Exception(response.status_code, response.text)
+        return response.json()
+
+    @staticmethod
+    def build_headers():
+        headers = {"Authorization": "Bearer " + bearer_token}
+        return headers
