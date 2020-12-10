0c4bb726276f833f53fcc4bc419ac02d868c9c90
Roxanica123 <irimia.andreea.roxana@gmail.com>
2020-12-10 21:57:50 +0200

    Extract the text and the coordinates from the retrieved tweets

---
+++ data_preparation.py
@@ -0,0 +1,46 @@
+from twitter_request import TwitterRequest
+
+MAX_REQUESTS = 100
+
+
+def get_recent_tweets_with_available_location(hashtag, wanted_results):
+    twitter = TwitterRequest(hashtag, wanted_results)
+    tweets_with_available_location = []
+    places = []
+    number_of_requests = 1
+    next_token = None
+    while len(tweets_with_available_location) < wanted_results and number_of_requests < MAX_REQUESTS:
+        response = twitter.send_request(next_token)
+        data = response["data"]
+        meta = response["meta"]
+        for tweet in data:
+            if tweet.get("geo") is not None and len(tweets_with_available_location) < wanted_results:
+                tweets_with_available_location.append(tweet)
+        next_token = meta["next_token"]
+        number_of_requests += 1
+        if response.get("includes") is not None and response["includes"].get("places") is not None:
+            places = places + response["includes"]["places"]
+    return extract_coordinates_from_tweets_info(tweets_with_available_location, places)
+
+
+def calculate_coordinates(place):
+    bbox = place["geo"]["bbox"]
+    return [(bbox[0] + bbox[2]) / 2, (bbox[1] + bbox[3]) / 2]
+
+
+def find_tweet_place_in_places_list(tweet, places):
+    place_id = tweet["geo"]["place_id"]
+    return next(filter(lambda current_place: current_place["id"] == place_id, places))
+
+
+def extract_coordinates_from_tweets_info(tweets, places):
+    tweets_with_extracted_location = []
+    for tweet in tweets:
+        if tweet["geo"].get("coordinates") is not None:
+            coordinates = tweet["coordinates"]
+        else:
+            place = find_tweet_place_in_places_list(tweet, places)
+            coordinates = calculate_coordinates(place)
+        tweet_with_extracted_location = {"text": tweet["text"], "coordinates": coordinates}
+        tweets_with_extracted_location.append(tweet_with_extracted_location)
+    return tweets_with_extracted_location
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from tweets import get_recent_tweets_with_available_location
+from data_preparation import get_recent_tweets_with_available_location
 from user_input import get_input_hashtag
 
-WANTED_RESULTS = 5
+WANTED_RESULTS = 2
 
 
 def main():
     hashtag = get_input_hashtag()
-    get_recent_tweets_with_available_location(hashtag, WANTED_RESULTS)
+    print(get_recent_tweets_with_available_location(hashtag, WANTED_RESULTS))
 
 
 if __name__ == "__main__":
