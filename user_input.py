c8e672cb564402665b92202e5bb6c75f9343ea46
Roxanica123 <irimia.andreea.roxana@gmail.com>
2020-12-12 13:02:24 +0200

    Added functions for ui query input validation

--- user_input.py
+++ user_input.py
@@ -1,6 +1,8 @@
 import re
 import sys
-from urllib.parse import quote
+from urllib.parse import quote, unquote
+
+MAX_WANTED_RESULTS = 1000
 
 
 def check_hashtag_validity(hashtag):
@@ -8,12 +10,27 @@ def check_hashtag_validity(hashtag):
     return False if match is None else True
 
 
+def check_wanted_results_validity(wanted_results):
+    try:
+        wanted_results = int(wanted_results)
+        if wanted_results < MAX_WANTED_RESULTS:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
 def assure_hashtag_sign_existence(hashtag):
-    return hashtag if (len(hashtag) > 0 and hashtag[0] == '#') else '#' + hashtag
+    return hashtag if (hashtag is None or (len(hashtag) > 0 and hashtag[0] == '#')) else '#' + hashtag
 
 
 def escape_hashtag_sign(hashtag):
     return quote(hashtag)
+
+
+def unescape_hashtag(hashtag):
+    return unquote(hashtag) if hashtag is not None else None
 
 
 def get_input_hashtag():
