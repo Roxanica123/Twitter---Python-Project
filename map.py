0fbf64aa96c7050c67e23e75b26147352ede8687
Roxanica123 <irimia.andreea.roxana@gmail.com>
2020-12-14 23:51:30 +0200

    Make markers have different colors

--- map.py
+++ map.py
@@ -1,11 +1,14 @@
+import random
+
 import folium
 
 
 class Map:
     def __init__(self, tweets_data):
+        self.color_theme = ['#00FFFF', '#00EFFF', '#00DEFF', '#00CBFF', '#00B8FF', '#00A3FF', '#008CFF']
         self.tweets_data = tweets_data
         self.centre_location = self.get_center_location()
-        self.map = folium.Map(location=self.centre_location, tiles='Stamen Toner', zoom_start=1)
+        self.map = folium.Map(location=self.centre_location, tiles='Stamen Toner', zoom_start=2)
         self.add_circles()
 
     def get_center_location(self):
@@ -19,8 +22,9 @@ class Map:
     def add_circles(self):
         for tweet in self.tweets_data:
             popup = tweet["text"]
-            folium.CircleMarker(radius=50, location=tweet["coordinates"][::-1], popup=popup, color='#3186cc',
-                                fill=True, fill_color='#3186cc').add_to(self.map)
+            color = random.choice(self.color_theme)
+            folium.CircleMarker(radius=50, location=tweet["coordinates"][::-1], popup=popup, color=color,
+                                fill=True, fill_color=color).add_to(self.map)
 
     def save_map(self, save_location):
         self.map.save(save_location)
